2dfd509de3b1d2508c9f7b848b0ad4121af6ebff
Thorsten Korpitsch <thorsten.korpitsch@gmail.com>
2020-12-09 17:04:34 +0100

    added hello world

---
+++ sources/main.cpp
@@ -0,0 +1,144 @@
+#include <gvk.hpp>
+#include <imgui.h>
+
+class lod_mesh_shader : public gvk::invokee
+{
+public: // v== cgb::invokee overrides which will be invoked by the framework ==v
+	lod_mesh_shader(avk::queue& aQueue) : mQueue{ &aQueue }
+	{}
+
+	void initialize() override
+	{
+		// Print some information about the available memory on the selected physical device:
+		gvk::context().print_available_memory_types();
+
+		// Create a graphics pipeline:
+		mPipeline = gvk::context().create_graphics_pipeline_for(
+			avk::vertex_shader("shaders/a_triangle.vert"),
+			avk::fragment_shader("shaders/a_triangle.frag"),
+			avk::cfg::front_face::define_front_faces_to_be_clockwise(),
+			avk::cfg::viewport_depth_scissors_config::from_framebuffer(gvk::context().main_window()->backbuffer_at_index(0)),
+			avk::attachment::declare(gvk::format_from_window_color_buffer(gvk::context().main_window()), avk::on_load::clear, avk::color(0), avk::on_store::store) // But not in presentable format, because ImGui comes after
+		);
+
+#if _DEBUG
+		mPipeline.enable_shared_ownership(); // Make it usable with the updater
+		mUpdater.on(
+			gvk::swapchain_resized_event(gvk::context().main_window()),
+			gvk::shader_files_changed_event(mPipeline)
+		)
+			.update(mPipeline);
+
+		gvk::current_composition()->add_element(mUpdater);
+#endif
+
+		auto imguiManager = gvk::current_composition()->element_by_type<gvk::imgui_manager>();
+		if (nullptr != imguiManager) {
+			imguiManager->add_callback([]() {
+
+				ImGui::Begin("Hello, world!");
+				ImGui::SetWindowPos(ImVec2(1.0f, 1.0f), ImGuiCond_FirstUseEver);
+				ImGui::Text("%.3f ms/frame", 1000.0f / ImGui::GetIO().Framerate);
+				ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
+
+				static std::vector<float> values;
+				values.push_back(1000.0f / ImGui::GetIO().Framerate);
+				if (values.size() > 90) {
+					values.erase(values.begin());
+				}
+				ImGui::PlotLines("ms/frame", values.data(), values.size(), 0, nullptr, 0.0f, FLT_MAX, ImVec2(0.0f, 100.0f));
+				ImGui::End();
+			});
+		}
+	}
+
+	void update() override
+	{
+		// On H pressed,
+		if (gvk::input().key_pressed(gvk::key_code::h)) {
+			// log a message:
+			LOG_INFO_EM("Hello Gears-Vk! Hello Auto-Vk!");
+		}
+
+		// On C pressed,
+		if (gvk::input().key_pressed(gvk::key_code::c)) {
+			// center the cursor:
+			auto resolution = gvk::context().main_window()->resolution();
+			gvk::context().main_window()->set_cursor_pos({ resolution[0] / 2.0, resolution[1] / 2.0 });
+		}
+
+		// On Esc pressed,
+		if (gvk::input().key_pressed(gvk::key_code::escape)) {
+			// stop the current composition:
+			gvk::current_composition()->stop();
+		}
+	}
+
+	void render() override
+	{
+		auto mainWnd = gvk::context().main_window();
+
+		// Get a command pool to allocate command buffers from:
+		auto& commandPool = gvk::context().get_command_pool_for_single_use_command_buffers(*mQueue);
+
+		// Create a command buffer and render into the *current* swap chain image:
+		auto cmdBfr = commandPool->alloc_command_buffer(vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
+		cmdBfr->begin_recording();
+		cmdBfr->begin_render_pass_for_framebuffer(gvk::context().main_window()->get_renderpass(), gvk::context().main_window()->current_backbuffer());
+		cmdBfr->handle().bindPipeline(vk::PipelineBindPoint::eGraphics, mPipeline->handle());
+		cmdBfr->handle().draw(3u, 1u, 0u, 0u);
+		cmdBfr->end_render_pass();
+		cmdBfr->end_recording();
+
+		// The swap chain provides us with an "image available semaphore" for the current frame.
+		// Only after the swapchain image has become available, we may start rendering into it.
+		auto imageAvailableSemaphore = mainWnd->consume_current_image_available_semaphore();
+
+		// Submit the draw call and take care of the command buffer's lifetime:
+		mQueue->submit(cmdBfr, imageAvailableSemaphore);
+		mainWnd->handle_lifetime(avk::owned(cmdBfr));
+	}
+
+private: // v== Member variables ==v
+
+	avk::queue* mQueue;
+	avk::graphics_pipeline mPipeline;
+#if _DEBUG
+	gvk::updater mUpdater;
+#endif
+
+}; // draw_a_triangle_app
+
+int main() // <== Starting point ==
+{
+	try {
+		// Create a window and open it
+		auto mainWnd = gvk::context().create_window("LOD Mesh Shaders");
+		mainWnd->set_resolution({ 1920, 1080 });
+		mainWnd->enable_resizing(true);
+		mainWnd->set_presentaton_mode(gvk::presentation_mode::mailbox);
+		mainWnd->set_number_of_concurrent_frames(3u);
+		mainWnd->open();
+
+		auto& singleQueue = gvk::context().create_queue({}, avk::queue_selection_preference::versatile_queue, mainWnd);
+		mainWnd->add_queue_family_ownership(singleQueue);
+		mainWnd->set_present_queue(singleQueue);
+
+		// Create an instance of our main cgb::element which contains all the functionality:
+		auto app = lod_mesh_shader(singleQueue);
+		// Create another element for drawing the UI with ImGui
+		auto ui = gvk::imgui_manager(singleQueue);
+
+		// GO:
+		gvk::start(
+			gvk::application_name("Hello, Gears-Vk + Auto-Vk World!"),
+			mainWnd,
+			app,
+			ui
+		);
+	}
+	catch (gvk::logic_error&) {}
+	catch (gvk::runtime_error&) {}
+	catch (avk::logic_error&) {}
+	catch (avk::runtime_error&) {}
+}
